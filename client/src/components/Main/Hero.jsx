168a745e23287a1bdf0e87ffbf459895c6b588a6
yashtarate2002 <yashtarate2002@gmail.com>
2024-05-08 11:12:24 +0530

    responsive

--- client/src/components/Main/Hero.jsx
+++ client/src/components/Main/Hero.jsx
@@ -4,7 +4,7 @@ import Stars from "../../assets/stars.svg";
 import Carousel from "../Primary/Carousel.jsx";
 import Button from "../Primary/PrimaryButton.jsx";
 import { LuArrowRight } from "react-icons/lu";
-import '../Registration/Registration-Success/RegistrationSuccess.css'
+import '../Main/Main.css'
 function ArrowRight() {
   return <LuArrowRight size={20} />;
 }
@@ -24,11 +24,12 @@ export default function Hero() {
       <p style={{color:'#1C1C1C',fontFamily:'Manrope'}} className="text-5xl font-semibold text-center">
         Every success journey <br></br> we’ve encountered.
       </p>
-      <div className="mt-20 flex">
+      <div className="heroicon mt-20 flex">
         <div className="w-2/3">
           <div className="relative w-[70%] mx-auto">
             <img src={Heroicon} alt="hero" className="" />
-            <div className="absolute top-20 -left-24 bg-white rounded-3xl pb-5 pt-8 px-5 max-w-[200px] shadow-sm shadow-black/20">
+            {/* achieved */}
+            <div className="achieved  absolute top-20 -left-24 bg-white rounded-3xl pb-5 pt-8 px-5 max-w-[200px] shadow-sm shadow-black/20">
               <div className="relative">
                 <img
                   src={Stars}
@@ -42,26 +43,33 @@ export default function Hero() {
                 availability
               </p>
             </div>
-            <div className="absolute bottom-10 -left-16 flex gap-2 bg-white rounded-full p-5 max-w-[250px] shadow-sm shadow-black/20">
+            {/* staff */}
+            <div className="staff">
+            <div className=" absolute bottom-10 -left-16 flex gap-2 bg-white rounded-full p-5 max-w-[250px] shadow-sm shadow-black/20">
               <img src={Rocket} alt="rocket" />
               <div>
                 <span style={{color:'#1C1C1C',fontFamily:'Manrope'}} className="font-semibold">10 DAYS</span>
                 <p style={{color:'#828282',fontFamily:'Manrope',fontWeight:'500'}}>Staff Deployment</p>
               </div>
             </div>
-            <div className="bg-[#002E18] absolute bottom-10 right-0 flex flex-col gap-2 rounded-3xl px-5 py-10 max-w-[250px] shadow-sm shadow-black/20">
-              <div className="space-x-2">
-                <span className="text-white font-bold text-4xl">$0.5</span>
-                <span className="text-[#CCCCCC]">MILLION</span>
+            </div>
+
+            {/* million */}
+            <div className="million">
+            <div className=" bg-[#002E18] absolute bottom-10 right-0 flex flex-col gap-2 rounded-3xl px-5 py-10 max-w-[250px] shadow-sm shadow-black/20">
+              <div className="million space-x-2">
+                <span className="  text-white font-bold text-4xl">$0.5</span>
+                <span className="  text-[#CCCCCC]">MILLION</span>
               </div>
-              <p style={{fontFamily:"Manrope",fontSize:'18px'}} className="text-[#CCCCCC]">
+              <p style={{fontFamily:"Manrope",fontSize:'18px'}} className="  text-[#CCCCCC]">
                 Reduced client expenses by saving on hiring and employee costs.
               </p>
             </div>
-          </div>
+            </div>
+          </div>  
         </div>
-        <div className="space-y-20 mt-20 w-1/3">
-          <Carousel  data={DUMMY_CAROUSEL_DATA} />
+        <div className="Explore space-y-20 mt-20 w-1/3">
+          <Carousel className="carousel" data={DUMMY_CAROUSEL_DATA} />
           <Button icon={ArrowRight} styleName="primary" style={{height:'60px'}}>
             Explore More
           </Button>
